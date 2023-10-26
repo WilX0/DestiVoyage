3b379fa7bfcc0eeb13312d276bb6452d786cb304
mkenzi06 <87369419+mkenzi06@users.noreply.github.com>
2023-10-26 19:04:29 +0200

    Test API meteo

---
+++ DestiVoyage/APIMETEO/testmeteo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>METEO</h1><br>
+    <span id="ville">Bejaia</span><br>
+    <span id="temperature"></span>°C
+
+    <button id="changer">changer de ville</button>
+
+
+
+
+    <script src="apimeteo.js"></script>
+</body>
+</html>
