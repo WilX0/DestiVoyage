177c3e8e71f97b80205345acc9f3e368b64b73dc
WilX0 <129814502+WilX0@users.noreply.github.com>
2023-11-08 19:52:33 +0100

    Add files via upload

---
+++ DestiVoyage/lefooter.php
@@ -0,0 +1,29 @@
+<footer class="footer py-3 my-4" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <img src="images/Logo.png" class="navbar-brand" alt="Logo DestiVoyage" id="logofooter">
+                </div>
+            </div>
+        </div>
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="#" class="navfooter nav-link px-2 text-body-secondary">Destinations</a></li>
+            <li class="nav-item"><a href="#" class="navfooter nav-link px-2 text-body-secondary">Blog</a></li>
+            <li class="nav-item"><a href="#" class="navfooter nav-link px-2 text-body-secondary">Les Meilleurs Vols</a>
+            </li>
+            <li class="nav-item"><a href="#" class="navfooter nav-link px-2 text-body-secondary">Nous contacter</a>
+            </li>
+            <li class="nav-item"><a href="#" class="navfooter nav-link px-2 text-body-secondary">About</a></li>
+        </ul>
+        <h4 id="slogan">Découverte,inspiration,voyage</h4>
+        <p class="text-center text-body-secondary">&copy; 2023 Made by The Crew</p>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <a href="#"><img src="images/facebook.png" alt="icone facebook"></a>
+                    <a href="#"><img src="images/pinterest.png" alt="icone pinterest"></a>
+                    <a href="#"><img src="images/instagram.png" alt="icone instagram"></a>
+                </div>
+            </div>
+        </div>
+    </footer>
