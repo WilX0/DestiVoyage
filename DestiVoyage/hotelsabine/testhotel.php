bd26aaa84f0c905ae4f855d4d63e3bdf136a5db0
mkenzi06 <87369419+mkenzi06@users.noreply.github.com>
2023-11-29 10:52:56 +0100

    hôtel (maquette)

---
+++ DestiVoyage/hotelsabine/testhotel.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOTEL</title>
+</head>
+<body>
+<h1>Recherche d'hôtels</h1>
+  <label for="cityCode">Code de la ville:</label>
+  <input type="text" id="cityCode" placeholder="PAR">
+  <button id="searchButton">Rechercher les hôtels</button>
+  <div id="hotelList"></div>
+    <script src="hoteljsapi.js"></script>
+</body>
+</html>
