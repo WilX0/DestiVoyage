1c495606745c39a4d68371f45d64d1b7aecbf7f2
mkenzi06 <87369419+mkenzi06@users.noreply.github.com>
2023-12-08 09:09:26 +0100

    hello3

---
+++ DestiVoyage/navlogin.php
@@ -0,0 +1,27 @@
+
+<nav class="lanav navbar navbar-expand-lg navbar-dark-5-strong">
+    <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="images/Logo.png" class="navbar-brand" alt="Logo DestiVoyage" id="idlogo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link text-white" aria-current="page" href="#">Profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="Book1.php">Vols</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Hotels</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Contact</a>
+                </li>
+            </ul>
+            <a href="deconnexion.php" class="mybtns btncarousel btn btn-primary">Se déconnecter</a>
+            
+        </div>
+    </div>
+</nav>
---
+++ DestiVoyage/navnotlogin.php
@@ -0,0 +1,28 @@
+<nav class="lanav navbar navbar-expand-lg navbar-dark-5-strong">
+            <div class="container">
+                <a class="navbar-brand" href="accueil"><img src="images/Logo.png" class="navbar-brand" alt="Logo DestiVoyage" id="idlogo"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <!-- <li class="nav-item">
+                            <a class="nav-link text-white" aria-current="page" href="#">Profil</a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="Book1.php">Vols</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Hotels</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Contact</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex">
+                    <a href="connexion"  class="mybtns btncarousel btn btn-primary">Se connecter</a> 
+                    <a href="inscription"  class="mybtns btncarousel btn btn-primary">S'inscrire</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
